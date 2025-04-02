bfe7a6f8cc7857e140f55602d40a17809f44a71e
V4ldum <56064897+V4ldum@users.noreply.github.com>
2025-04-02 16:39:10 +0200

    fix

--- bin/mock/bingo/database_mock_repository.dart
+++ bin/mock/bingo/database_mock_repository.dart
@@ -4,14 +4,7 @@ import 'dart:math' show Random;
 import 'package:bingo/models/bingo.dart';
 import 'package:bingo/repositories/database_repository.dart';
 import 'package:bingo/repositories/dtos/bingo_dto.dart';
-import 'package:bingo/router.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/widgets.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:rust/rust.dart';
-import 'package:shadcn_ui/shadcn_ui.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'fake_supabase_auth.dart';
 
