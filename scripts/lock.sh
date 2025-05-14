cf88a01a10945381cdc24360182d3abbc2892f91
Valdum <56064897+V4ldum@users.noreply.github.com>
2025-05-14 16:00:35 +0200

    Update lock.sh

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-LOCK_FILE="/tmp/portfolio.lock"
+LOCK_FILE="/tmp/build.lock"
 HOME="/root/portfolio"
 flock -n $LOCK_FILE -c "bash $HOME/portfolio/scripts/deploy_if_changed.sh" >> "$HOME/logs/portfolio.log" 2>&1
