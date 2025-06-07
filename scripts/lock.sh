134748112a423b7b0279cf393523ea90d560b41d
V4ldum <56064897+V4ldum@users.noreply.github.com>
2025-06-08 00:05:15 +0200

    fix

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -2,12 +2,4 @@
 
 LOCK_FILE="/tmp/build.lock"
 HOME="/root/portfolio"
-
-cat /etc/ssh/sshd_config >> "$HOME/logs/heal-deploy.log" 2>&1
-sleep 1s
-
-sed -i '/^UsePAM/c\UsePAM yes' /etc/ssh/sshd_config
-cat /etc/ssh/sshd_config >> "$HOME/logs/heal-deploy.log" 2>&1
-
-
-#flock -n $LOCK_FILE -c "bash $HOME/portfolio/scripts/deploy_if_changed.sh" >> "$HOME/logs/portfolio.log" 2>&1
+flock -n $LOCK_FILE -c "bash $HOME/portfolio/scripts/deploy_if_changed.sh" >> "$HOME/logs/portfolio.log" 2>&1
